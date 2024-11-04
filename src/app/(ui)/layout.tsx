dbfb565274122d57c6053a58504ec9f65c19d52c
LucasAlvaresA <lucas.alvaress@hotmail.com>
2024-11-04 18:56:31 -0300

    feat: create trends components

--- src/app/(ui)/layout.tsx
+++ src/app/(ui)/layout.tsx
@@ -3,6 +3,7 @@ import { NavLogout } from "@/components/nav/nav-logout";
 import { NavMyProfile } from "@/components/nav/nav-myprofile";
 import { Logo } from "@/components/ui/logo";
 import { SearchInput } from "@/components/ui/search-input";
+import { TrendingArea } from "@/components/ui/trending-area";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { ReactNode } from "react";
@@ -38,6 +39,7 @@ export default function Layout({ children }: Props) {
             <section className="flex-1 max-w-lg">{children}</section>
             <aside className="hidden lg:flex flex-col gap-6 sticky top-0 h-fit w-96 px-8 py-6 border-l-2 border-gray-900">
                 <SearchInput hideOnSearch />
+                <TrendingArea />
             </aside>
         </main>
     );
