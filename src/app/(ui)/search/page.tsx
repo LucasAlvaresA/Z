093f86ab8ec739d03186af7e6059e6bfb907433a
LucasAlvaresA <lucas.alvaress@hotmail.com>
2024-11-06 13:34:25 -0300

    fix: change the favicon and fix a error on search route

--- src/app/(ui)/search/page.tsx
+++ src/app/(ui)/search/page.tsx
@@ -2,21 +2,12 @@ import { TweetItem } from "@/components/tweet/tweet.item";
 import { GeneralHeader } from "@/components/ui/general-header";
 import { SearchInput } from "@/components/ui/search-input";
 import { searchExampleTweet } from "@/data/tweet";
-import { redirect } from "next/navigation";
 
-type Props = {
-    searchParams: {
-        q: string | undefined;
-    };
-};
-
-export default function Page({ searchParams }: Props) {
-    if (!searchParams.q) redirect("/");
-
+export default function Page() {
     return (
         <div>
             <GeneralHeader backHref="/">
-                <SearchInput defaultValue={searchParams.q} />
+                <SearchInput />
             </GeneralHeader>
             <div className="border-t-2 border-gray-900">
                 <TweetItem tweet={searchExampleTweet} />
